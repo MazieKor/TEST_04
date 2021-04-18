68c657ca5bdde7e33871a543be7beef6e0f12565
MazieKor <maciej.coders@gmail.com>
2021-04-19 01:03:18 +0200

    mess

--- src/pl/coderslab/Main.java
+++ src/pl/coderslab/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("44");
         System.out.println("52");
-
+        System.out.println("67");
 
     }
 }
