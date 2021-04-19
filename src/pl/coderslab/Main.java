cab70f0bc50bf8a17b2890de25c4de64451a2c29
MazieKor <69371099+MazieKor@users.noreply.github.com>
2021-04-19 19:09:13 +0200

    Zmiana nr 4

--- src/pl/coderslab/Main.java
+++ src/pl/coderslab/Main.java
@@ -3,6 +3,6 @@ package pl.coderslab;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here Zmiana na masterze. Zmiana nr 3
+	// write your code here Zmiana na masterze. Zmiana nr 4
     }
 }
