017366222725c969efdffb090a49fdc407c2cfbd
MazieKor <69371099+MazieKor@users.noreply.github.com>
2021-04-19 18:32:36 +0200

    Zmiana nr 2

--- src/pl/coderslab/Main.java
+++ src/pl/coderslab/Main.java
@@ -3,6 +3,6 @@ package pl.coderslab;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here Zmiana na thirdzie
+	// write your code here Zmiana na thirdzie. Zmiana nr 2
     }
 }
