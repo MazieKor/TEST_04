1aa942b251922db7bd2f2616e83aea0c7cf8f2ed
MazieKor <69371099+MazieKor@users.noreply.github.com>
2021-04-19 19:08:57 +0200

    Zmiana nr 4

--- src/pl/coderslab/Main.java
+++ src/pl/coderslab/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("uwaga34");
-        // write your code here Zmiana na secondzie. Zmiana nr 3
+        // write your code here Zmiana na secondzie. Zmiana nr 4
     }
 }
